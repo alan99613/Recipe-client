0e3c42a5cf8fcfb6ed324054886179a4d0910724
Wong Pui Lam <alanwong9961@gmail.com>
2021-05-13 18:23:35 +0800

    Added navigation bar

--- src/router/router.js
+++ src/router/router.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router"
 import index from '../views/index.vue'
+import navbar from '../views/navbar.vue'
 
 const routes = [
   {
     path: "/",
     components: {
-      default: index
+      default: index,
+      nav: navbar
     }
   }
 ]
---
+++ src/views/navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="navbar">
+    <div class="barItem"><router-link to="/">Home</router-link></div>
+    <div class="barItem barRightItem"><a href="" @click.prevent="loginout">{{ login? 'Logout':'Login' }}</a></div>
+    <div class="barItem barRightItem"><router-link v-if="login" to="/userinfo">User Info</router-link></div>
+    <div class="barItem barRightItem"><router-link v-if="login" to="/favourite">Favourite</router-link></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        login: false
+      }
+    }, methods: {
+      loginout() {
+        if (this.login) {
+          this.$router.push('/logout')
+        } else {
+          this.$router.push('/login')
+        }
+      }
+    }, created() {
+      //Check if the user is logged in
+      if (localStorage.token) {
+        this.login = true
+      }
+    }
+  }
+</script>
